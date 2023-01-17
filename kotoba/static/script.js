8f2bc433bad6f8323bdcb4cfadaf89bf9f0d6ce4
Emmie Maeda <emmie.maeda@gmail.com>
2023-01-16 20:31:27 -0500

    Begin static data.

---
+++ kotoba/static/script.js
@@ -0,0 +1,19 @@
+function request(method, route, data, callback) {
+  fetch(route, {
+    method,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }).then(callback);
+}
+
+function nextCard() {
+  request('GET', '/card/next', null, function(response) {
+    // TODO
+  });
+}
+
+function answer(cardId, action) {
+  request('POST', '/card/answer', { cardId, action }, function(response) {
+    // TODO
+  });
+}
